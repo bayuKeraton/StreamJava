d874a80351f4081693aa6882f7453962342aa531
Bayu Kembara <bayu@teavaro.com>
2023-04-10 16:07:28 +0700

    55:44

--- src/test/java/org/example/LazyValuationTest.java
+++ src/test/java/org/example/LazyValuationTest.java
@@ -7,13 +7,42 @@ import java.util.stream.Stream;
 
 public class LazyValuationTest {
     @Test
-    void testIntermediateOperation(){
-    List<String> names = List.of("a", "b", "c","d", "e");
-    
-    Stream<String> upper = names.stream()
-            .map(name-> {
-                System.out.println("Change " + name + " to UPPERCASE");
-                return name.toUpperCase();
-            });
+    void testIntermediateOperation() {
+        List<String> names = List.of("a", "b", "c", "d", "e");
+
+        Stream<String> upper = names.stream()
+                .map(name -> {
+                    System.out.println("Change " + name + " to UPPERCASE");
+                    return name.toUpperCase();
+                });
+
+    }
+
+    @Test
+    void terminalOperation() {
+        List<String> names = List.of("a", "b", "c", "d", "e");
+
+        Stream<String> upper = names.stream()
+                .map(name -> {
+                    System.out.println("Change " + name + " to UPPERCASE");
+                    return name.toUpperCase();
+                });
+
+        upper.forEach(uppers -> {
+                    System.out.println("PrintName : " + uppers);
+                }
+        );
+
+//        ! OR
+        names.stream()
+                .map(nama -> {
+                    System.out.println("Change " + nama + " to UPPERCASE");
+                    return nama.toUpperCase();
+                })
+                .map(trans -> {
+                    System.out.println("Add " + trans + " From Name");
+                    return "Mr " + trans;
+                })
+                .forEach(System.out::println);
     }
 }
